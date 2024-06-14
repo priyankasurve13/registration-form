bc03cfb10e354e70d770b2f09b01edfc69513beb
Priyanka <survepriyanka113@gmail.com>
2024-06-14 19:35:28 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,65 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
+const dotenv = require("dotenv");
+
+const app = express();
+dotenv.config();
+
+const port = process.env.PORT || 3000;
+const username = process.env.MONGODB_USERNAME;
+const password = process.env.MONGODB_PASSWORD;
+const dbName = process.env.MONGODB_DBNAME || 'your_database_name_here';
+const dbCluster = 'cluster0.ihj6bxv'; // Your MongoDB cluster
+
+const mongoURI = `mongodb+srv://${username}:${password}@${dbCluster}.mongodb.net/${dbName}?retryWrites=true&w=majority`;
+
+mongoose.connect(mongoURI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+}).then(() => {
+    console.log('Connected to MongoDB');
+}).catch(error => {
+    console.error('Error connecting to MongoDB:', error);
+});
+
+const registrationSchema = new mongoose.Schema({
+    name: String,
+    email: String,
+    password: String
+});
+const Registration = mongoose.model("Registration", registrationSchema);
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+app.get("/", (req, res) => {
+    res.sendFile(__dirname + "/pages/index.html");
+});
+
+app.post("/register", async (req, res) => {
+    try {
+        const { name, email, password } = req.body;
+        const registrationData = new Registration({ name, email, password });
+        await registrationData.save();
+        res.redirect("/success");
+    } catch (error) {
+        console.error(error);
+        res.redirect("/error");
+    }
+});
+
+app.get("/success", (req, res) => {
+    res.sendFile(__dirname + "/pages/success.html");
+});
+
+app.get("/error", (req, res) => {
+    res.sendFile(__dirname + "/pages/error.html");
+});
+
+app.listen(port, () => {
+    console.log(`Server is running on port ${port}`);
+});
+
+
+
